7c70daa22ff32a5fa06b18baf59e88e289418e4b
Aleksander Bekrenev <praise2001@yandex.ru>
2022-11-08 20:17:29 +0700

    Initial Commit

---
+++ EHM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EHM
+//
+//  Created by Александр Бекренев on 08.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
