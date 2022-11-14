8b4fc7ad3c0a22e07d97ec075dc92680b96b9c57
Aleksander Bekrenev <praise2001@yandex.ru>
2022-11-14 14:45:22 +0700

    feat: working with backend api
    implemented search by handling textField value

---
+++ EHM/Services/SearchService/SearchService.swift
@@ -0,0 +1,56 @@
+//
+//  SearchService.swift
+//  EHM
+//
+//  Created by Александр Бекренев on 13.11.2022.
+//
+
+import Foundation
+
+class SearchService: SearchServiceProtocol {
+    private enum SearchError: Error {
+        case parsingJSON
+    }
+    
+    private let networkClient: NetworkClient
+    private let backendIP = ProcessInfo.processInfo.environment["BACKEND_IP"]
+    private let backendPORT = ProcessInfo.processInfo.environment["BACKEND_PORT"]
+    
+    weak var delegate: SearchServiceDelegate?
+    
+    init(delegate: SearchServiceDelegate?) {
+        self.delegate = delegate
+        self.networkClient = NetworkClient.shared
+    }
+    
+    public func requestSearch(with request: String) {
+        let dashedRequest = request.replacingOccurrences(of: " ", with: "-")
+        guard
+            let ip = backendIP,
+            let port = backendPORT,
+            let url = URL(string: "http://\(ip):\(port)/search/\(dashedRequest)") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        
+        networkClient.fetch(url: url) { [weak self] result in
+            DispatchQueue.global().async { [weak self] in
+                guard let self = self else { return }
+                switch result {
+                case .failure(let error):
+                    print(error)
+                case .success(let data):
+                    guard let searchResult = try? JSONDecoder().decode(SearchResult.self, from: data) else {
+                        print(SearchError.parsingJSON)
+                        return
+                    }
+                    
+                    DispatchQueue.main.async { [weak self] in
+                        guard let self = self else { return }
+                        print(searchResult)
+//                        self.delegate?.didRecieveSearchResult(result: searchResult)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ EHM/Services/SearchService/SearchServiceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  SearchServiceProtocol.swift
+//  EHM
+//
+//  Created by Александр Бекренев on 14.11.2022.
+//
+
+import Foundation
+
+protocol SearchServiceProtocol {
+    func requestSearch(with request: String)
+}
