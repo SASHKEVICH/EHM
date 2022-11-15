9eaec58665b9f77dffd7988effa9033205a228e3
Aleksander Bekrenev <praise2001@yandex.ru>
2022-11-15 18:16:55 +0700

    feat: table view delegating

---
+++ EHM/Presentation/Search/TableView/SearchViewController+UITableViewDataSource.swift
@@ -0,0 +1,41 @@
+//
+//  SearchViewController+UITableViewDataSource.swift
+//  EHM
+//
+//  Created by Александр Бекренев on 14.11.2022.
+//
+
+import Foundation
+import UIKit
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResult?.items.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let item = searchResult?.items[indexPath.item] else {
+            fatalError("something went wrong in cells...")
+        }
+        
+        switch item.type {
+        case .album:
+            if let cell = searchTableViewController.tableView.dequeueReusableCell(withIdentifier: "AlbumCell", for: indexPath) as? AlbumCell {
+                cell.item = item
+                return cell
+            }
+        case .band:
+            if let cell = searchTableViewController.tableView.dequeueReusableCell(withIdentifier: "BandCell", for: indexPath) as? BandCell {
+                cell.item = item
+                return cell
+            }
+        case .song:
+            if let cell = searchTableViewController.tableView.dequeueReusableCell(withIdentifier: "SongCell", for: indexPath) as? SongCell {
+                cell.item = item
+                return cell
+            }
+        }
+        
+        return UITableViewCell()
+    }
+}
